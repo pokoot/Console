54a8f480d782af29daa9ca72a430261e5cb8ef6b
pokoot <pokoot@gmail.com>
2013-12-17 13:31:37 +0800

    new updates

---
+++ tests/ConsoleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Goldfinger;
+
+
+class ConsoleTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testTrue()
+    {
+        $foo = true;
+        $this->assertTrue($foo);
+    }
+
+}
+
